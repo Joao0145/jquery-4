4f8649ea57a2eefa211532584688558aa525f2d5
João Victor Alves <160748355+Joao0145@users.noreply.github.com>
2024-12-05 19:36:16 -0300

    subir repositório
    subir repositório

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+$(document).ready(function() {
+    // Quando o botao for clicado, alterna a viibilidade da div #extra-info
+    $('#toggle-info').click(function() {
+        $('#extra-info').toggle(); // Alterna entre mostrar e esconder
+    });
+});
